25bc73d9c57174f82e68f13af8b7031e5555e047
Wibowo Arindrarto <contact@arindrarto.dev>
2022-05-27 00:10:52 +0200

    feat(opml): Add OPML reading

---
+++ internal/opml/opml.go
@@ -0,0 +1,112 @@
+// package opml provides functionalities for parsing and writing OPML files.
+//
+// It follows the OPML 2.0 specifications [1], but keeps only tags relevant to processing
+// subscription lists. Elements relating to display settings, such as expansionState or
+// vertScrollState, are omitted.
+//
+// [1] http://opml.org/spec2.opml
+package opml
+
+import (
+	"bytes"
+	"encoding/xml"
+	"fmt"
+	"time"
+
+	"golang.org/x/net/html/charset"
+)
+
+// Parse parses the given raw OPML document into an OPML struct. Only version 2.0 is supported.
+func Parse(raw []byte) (*OPML, error) {
+	dec := xml.NewDecoder(bytes.NewReader(raw))
+	dec.CharsetReader = charset.NewReaderLabel
+
+	var doc OPML
+	if err := dec.Decode(&doc); err != nil {
+		return nil, err
+	}
+	if v := doc.Version; v != "2.0" {
+		return nil, fmt.Errorf("opml: version '%s' is unsupported", v)
+	}
+
+	return &doc, nil
+}
+
+// OPML represents the minimal contents of an OPML file required to for storing a subscription list.
+type OPML struct {
+	Body    Body   `xml:"body"`
+	Head    Head   `xml:"head"`
+	Version string `xml:"version,attr"`
+}
+
+// Head is the <head> element of an OPML file.
+type Head struct {
+	Title        *string     `xml:"title"`
+	DateCreated  *rfc822Time `xml:"dateCreated"`
+	DateModified *rfc822Time `xml:"dateModified"`
+	OwnerName    *string     `xml:"ownerName"`
+	OwnerEmail   *string     `xml:"ownerEmail"`
+	OwnerID      *string     `xml:"ownerID"`
+}
+
+// Body is the <body> element of an OPML file.
+type Body struct {
+	Outlines []*Outline `xml:"outline"`
+}
+
+// Outline is a single outline item in the OPML body. It represents a single subscription / feed.
+// Nesting is not supported.
+type Outline struct {
+	Text   string `xml:"text,attr"`
+	Type   string `xml:"type,attr"`
+	XMLURL string `xml:"xmlUrl,attr"`
+
+	Description *string `xml:"description,attr"`
+	HTMLURL     *string `xml:"htmlUrl,attr"`
+	Language    *string `xml:"language,attr"`
+	Title       *string `xml:"title,attr"`
+	Version     *string `xml:"version,attr"`
+}
+
+// rfc822Time wraps the time.Time struct for use in UnmarshalXML.
+type rfc822Time struct {
+	time.Time
+}
+
+// tsFormats is an array of possible time formats that can be found in an OPML file. These are
+// roughly based on RFC822, with variations in number of digits for day and year, and
+// presence/absence of minutes. When parsing, they are iterated over in-order.
+var tsFormats = []string{
+	"Mon, 02 Jan 2006 15:04:05 MST",
+	"Mon, 02 Jan 2006 15:04 MST",
+	"Mon, 02 Jan 06 15:04:05 MST",
+	"Mon, 02 Jan 06 15:04 MST",
+	"Mon, 2 Jan 2006 15:04:05 MST",
+	"Mon, 2 Jan 2006 15:04 MST",
+	"Mon, 2 Jan 06 15:04:05 MST",
+	"Mon, 2 Jan 06 15:04 MST",
+}
+
+func (t *rfc822Time) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
+
+	var raw string
+	_ = d.DecodeElement(&raw, &start)
+
+	var (
+		ts  time.Time
+		err error
+	)
+	for _, format := range tsFormats {
+		ts, err = time.Parse(format, raw)
+		if err == nil {
+			break
+		}
+	}
+	if ts.IsZero() {
+		return fmt.Errorf("opml: invalid time: %s", raw)
+	}
+
+	*t = rfc822Time{ts}
+
+	return nil
+}
---
+++ internal/opml/opml_test.go
@@ -0,0 +1,147 @@
+package opml
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestParseOkExtended(t *testing.T) {
+	raw := `<?xml version="1.0" encoding="ISO-8859-1"?>
+<opml version="2.0">
+  <head>
+    <title>mySubscriptions.opml</title>
+    <dateCreated>Sat, 18 Jun 2005 12:11:52 GMT</dateCreated>
+    <dateModified>Tue, 2 Aug 2005 21:42:48 GMT</dateModified>
+    <ownerName>Dave Winer</ownerName>
+    <ownerEmail>dave@scripting.com</ownerEmail>
+    <expansionState />
+    <vertScrollState>1</vertScrollState>
+    <windowTop>61</windowTop>
+    <windowLeft>304</windowLeft>
+    <windowBottom>562</windowBottom>
+    <windowRight>842</windowRight>
+    </head>
+  <body>
+    <outline
+        text="CNET News.com"
+        description="Tech news and business reports by CNET News.com. Focused on information technology, core topics include computers, hardware, software, networking, and Internet media."
+        htmlUrl="http://news.com.com/"
+        language="unknown"
+        title="CNET News.com"
+        type="rss"
+        version="RSS2"
+        xmlUrl="http://news.com.com/2547-1_3-0-5.xml"
+    />
+    <outline
+        text="NYT &gt; Business"
+        description="Find breaking news &amp; business news on Wall Street, media &amp; advertising, international business, banking, interest rates, the stock market, currencies &amp; funds."
+        htmlUrl="http://www.nytimes.com/pages/business/index.html?partner=rssnyt"
+        language="unknown"
+        title="NYT &gt; Business"
+        type="rss"
+        version="RSS2"
+        xmlUrl="http://www.nytimes.com/services/xml/rss/nyt/Business.xml"
+    />
+    <outline
+        text="Wired News"
+        description="Technology, and the way we do business, is changing the world we know. Wired News is a technology - and business-oriented news service feeding an intelligent, discerning audience. What role does technology play in the day-to-day living of your life? Wired News tells you. How has evolving technology changed the face of the international business world? Wired News puts you in the picture."
+        htmlUrl="http://www.wired.com/"
+        language="unknown"
+        title="Wired News"
+        type="rss"
+        version="RSS"
+        xmlUrl="http://www.wired.com/news_drop/netcenter/netcenter.rdf"
+    />
+    <outline
+        text="NYT &gt; Technology"
+        description=""
+        htmlUrl="http://www.nytimes.com/pages/technology/index.html?partner=rssnyt"
+        language="unknown"
+        title="NYT &gt; Technology"
+        type="rss"
+        version="RSS2"
+        xmlUrl="http://www.nytimes.com/services/xml/rss/nyt/Technology.xml"
+    />
+  </body>
+</opml>
+`
+
+	r := require.New(t)
+	a := assert.New(t)
+
+	doc, err := Parse([]byte(raw))
+	r.NoError(err)
+
+	r.NotNil(doc.Head)
+	head := doc.Head
+	//
+	a.Equal(stringp("mySubscriptions.opml"), head.Title)
+	//
+	a.Equal(2005, head.DateCreated.Year())
+	a.Equal(time.June, head.DateCreated.Month())
+	a.Equal(18, head.DateCreated.Day())
+	a.Equal(12, head.DateCreated.Hour())
+	a.Equal(11, head.DateCreated.Minute())
+	a.Equal(52, head.DateCreated.Second())
+	//
+	a.Equal(2005, head.DateModified.Year())
+	a.Equal(time.August, head.DateModified.Month())
+	a.Equal(2, head.DateModified.Day())
+	a.Equal(21, head.DateModified.Hour())
+	a.Equal(42, head.DateModified.Minute())
+	a.Equal(48, head.DateModified.Second())
+	//
+	r.NotNil(head.OwnerName)
+	a.Equal("Dave Winer", *head.OwnerName)
+	//
+	r.NotNil(head.OwnerEmail)
+	a.Equal("dave@scripting.com", *head.OwnerEmail)
+	//
+	a.Nil(head.OwnerID)
+
+	r.NotNil(doc.Body)
+	body := doc.Body
+	r.NotNil(body.Outlines)
+	outls := body.Outlines
+	r.Len(outls, 4)
+	//
+	outl0 := outls[0]
+	a.Equal("CNET News.com", outl0.Text)
+	a.Equal("rss", outl0.Type)
+	a.Equal("http://news.com.com/2547-1_3-0-5.xml", outl0.XMLURL)
+	r.NotNil(outl0.Description)
+	a.Equal(
+		"Tech news and business reports by CNET News.com."+
+			" Focused on information technology, core topics include computers, hardware, software,"+
+			" networking, and Internet media.",
+		*outl0.Description,
+	)
+	r.NotNil(outl0.HTMLURL)
+	a.Equal("http://news.com.com/", *outl0.HTMLURL)
+	r.NotNil(outl0.Language)
+	a.Equal("unknown", *outl0.Language)
+	r.NotNil(outl0.Title)
+	a.Equal("CNET News.com", *outl0.Title)
+	r.NotNil(outl0.Version)
+	a.Equal("RSS2", *outl0.Version)
+	//
+	outl3 := outls[3]
+	a.Equal("NYT > Technology", outl3.Text)
+	a.Equal("rss", outl3.Type)
+	a.Equal("http://www.nytimes.com/services/xml/rss/nyt/Technology.xml", outl3.XMLURL)
+	r.NotNil(outl3.Description)
+	a.Equal("", *outl3.Description)
+	r.NotNil(outl3.HTMLURL)
+	a.Equal("http://www.nytimes.com/pages/technology/index.html?partner=rssnyt", *outl3.HTMLURL)
+	r.NotNil(outl3.Language)
+	a.Equal("unknown", *outl3.Language)
+	r.NotNil(outl3.Title)
+	a.Equal("NYT > Technology", *outl3.Title)
+	r.NotNil(outl0.Version)
+	a.Equal("RSS2", *outl0.Version)
+}
+
+func stringp(value string) *string { return &value }
