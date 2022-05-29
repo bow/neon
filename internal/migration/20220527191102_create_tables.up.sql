a8499d44cd4240222239728ea2e98709e9c334bc
Wibowo Arindrarto <contact@arindrarto.dev>
2022-05-29 10:03:23 +0200

    feat(db): Add feed category as separate table

--- internal/migration/20220527191102_create_tables.up.sql
+++ internal/migration/20220527191102_create_tables.up.sql
@@ -5,8 +5,13 @@ CREATE TABLE IF NOT EXISTS
   , title TEXT NOT NULL
   , subtitle TEXT NULL
   , authors JSON NOT NULL DEFAULT '[]'
-  , categories JSON NOT NULL DEFAULT '[]'
   , update_time TIMESTAMP NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS
+  feed_categories
+  ( id INTEGER PRIMARY KEY AUTOINCREMENT
+  , name TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS
@@ -21,4 +26,11 @@ CREATE TABLE IF NOT EXISTS
   , publish_time TIMESTAMP NULL
   , update_time TIMESTAMP NOT NULL
   , FOREIGN KEY(feed_id) REFERENCES feeds(id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS
+  feeds_x_feed_categories
+  ( feed_id INTEGER NOT NULL
+  , feed_category_id INTEGER NOT NULL
+  , PRIMARY KEY (feed_id, feed_category_id)
 );
