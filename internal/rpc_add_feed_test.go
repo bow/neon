1c92207ba3c67e15536536d6581310ab316fc64f
Wibowo Arindrarto <contact@arindrarto.dev>
2022-06-15 00:08:44 +0200

    test: Expand AddFeed RPC test with extended test

--- internal/rpc_add_feed_test.go
+++ internal/rpc_add_feed_test.go
@@ -50,6 +50,55 @@ func TestAddFeedOkMinimal(t *testing.T) {
 	a.True(existf())
 }
 
+func TestAddFeedOkExtended(t *testing.T) {
+	t.Parallel()
+
+	r := require.New(t)
+	a := assert.New(t)
+
+	req := api.AddFeedRequest{
+		Url:         "http://foo.com/feed.xml",
+		Title:       stringp("user-title"),
+		Description: stringp("user-description"),
+		Categories:  []string{"cat-1", "cat-2"},
+	}
+	feed := gofeed.Feed{
+		Title:       "feed-title-original",
+		Description: "feed-description-original",
+		Link:        "https://foo.com",
+		FeedLink:    "https://foo.com/feed.xml",
+	}
+	parser := NewMockFeedParser(gomock.NewController(t))
+	parser.
+		EXPECT().
+		ParseURL(req.Url).
+		MaxTimes(1).
+		Return(&feed, nil)
+
+	client, db := setupOfflineTest(t, parser)
+
+	existf1 := func() bool {
+		return db.rowExists(feedExistSQL, feed.Title, feed.Description, feed.FeedLink, feed.Link)
+	}
+	existf2 := func() bool {
+		return db.rowExists(feedExistSQL, req.Title, req.Description, feed.FeedLink, feed.Link)
+	}
+
+	a.Equal(0, db.countFeeds())
+	a.Equal(0, db.countFeedCategories())
+	a.False(existf1())
+	a.False(existf2())
+
+	rsp, err := client.AddFeed(context.Background(), &req)
+	r.NoError(err)
+	r.NotNil(rsp)
+
+	a.Equal(1, db.countFeeds())
+	a.Equal(2, db.countFeedCategories())
+	a.False(existf1())
+	a.True(existf2())
+}
+
 // Query for checking that a feed row with the given columns exist.
 const feedExistSQL = `
 	SELECT
@@ -62,3 +111,5 @@ const feedExistSQL = `
 		AND xml_url = ?
 		AND html_url = ?
 `
+
+func stringp(value string) *string { return &value }
