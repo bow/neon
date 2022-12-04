2da291982c576fb5790cfc2f9ef7720194421b83
Wibowo Arindrarto <contact@arindrarto.dev>
2022-12-04 11:12:55 +0100

    refactor: Move cmd/*_test to one file

--- cmd/cmd_test.go
+++ cmd/cmd_test.go
@@ -5,9 +5,33 @@ package cmd
 
 import (
 	"bytes"
+	"fmt"
+	"testing"
 
 	"github.com/spf13/cobra"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/bow/iris/internal"
 )
+
+func TestNoArgs(t *testing.T) {
+	stdout, stderr, err := execCommand(nil)
+	require.NoError(t, err)
+
+	assert.Empty(t, stderr)
+	assert.Contains(t, stdout, "Feed reader suite")
+	assert.Contains(t, stdout, `Use "iris [command] --help" for more information`)
+}
+
+func TestVersionOk(t *testing.T) {
+	stdout, stderr, err := execCommand([]string{"version"})
+	require.NoError(t, err)
+
+	assert.Empty(t, stderr)
+	assert.Contains(t, stdout, "App        : iris")
+	assert.Contains(t, stdout, fmt.Sprintf("Version    : %s", internal.Version()))
+}
 
 // execCommand executes the command for testing.
 func execCommand(args []string) (stdout string, stderr string, err error) {
