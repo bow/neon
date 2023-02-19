8a7edc1971d563001f671b6ebb5c1a99c4df81ee
Wibowo Arindrarto <contact@arindrarto.dev>
2023-02-19 18:09:34 +0100

    feat: Update pretty log message style

--- internal/log.go
+++ internal/log.go
@@ -9,6 +9,7 @@ import (
 	"os"
 	"strings"
 	"time"
+	"unicode"
 
 	"github.com/rs/zerolog"
 	zlog "github.com/rs/zerolog/log"
@@ -36,7 +37,17 @@ func InitGlobalLog(
 
 	switch style {
 	case PrettyLogStyle:
-		cw = zerolog.ConsoleWriter{Out: writer, TimeFormat: tf}
+		cw = zerolog.ConsoleWriter{
+			Out:                 writer,
+			TimeFormat:          tf,
+			PartsExclude:        []string{"time"},
+			FormatLevel:         prettyLevelFormatter(),
+			FormatMessage:       prettyMessageFormatter(),
+			FormatFieldName:     prettyFieldNameFormatter(),
+			FormatFieldValue:    prettyFieldValueFormatter(),
+			FormatErrFieldName:  prettyErrFieldNameFormatter(),
+			FormatErrFieldValue: prettyErrFieldValueFormatter(),
+		}
 	case JSONLogStyle:
 		cw = writer
 	}
@@ -97,6 +108,119 @@ func (s *logConfigState) apply() {
 	zerolog.SetGlobalLevel(s.logLevel)
 	zlog.Logger = s.logger
 }
+
+// Adapted from:
+// https://github.com/rs/zerolog/blob/762546b5c64e03f3d23f867213e80aa45906aaf7/console.go
+func prettyLevelFormatter() zerolog.Formatter {
+
+	return func(i any) string {
+		var l string
+		if ll, ok := i.(string); ok {
+			switch ll {
+			case zerolog.LevelTraceValue:
+				l = colorizeLevel("TRC", colorWhite)
+			case zerolog.LevelDebugValue:
+				l = colorizeLevel("DBG", colorMagenta)
+			case zerolog.LevelInfoValue:
+				l = colorizeLevel("INF", colorCyan)
+			case zerolog.LevelWarnValue:
+				l = colorizeLevel("WRN", colorYellow)
+			case zerolog.LevelErrorValue:
+				l = colorizeLevel("ERR", colorRed)
+			case zerolog.LevelFatalValue:
+				l = colorizeLevel("FTL", colorRed)
+			case zerolog.LevelPanicValue:
+				l = colorizeLevel("PNC", colorRed)
+			default:
+				l = colorize(ll, colorBold)
+			}
+		} else {
+			if i == nil {
+				l = colorize("???", colorBold)
+			} else {
+				l = strings.ToUpper(fmt.Sprintf("%s", i))[0:3]
+			}
+		}
+		return l
+	}
+}
+
+func prettyMessageFormatter() zerolog.Formatter {
+	return func(i any) string {
+		if i != nil {
+			msg := fmt.Sprintf("%s", i)
+			// Use 'range' to get to the first character, which may span
+			// more than one byte.
+			for idx, c := range msg {
+				return colorize(
+					string(unicode.ToUpper(c))+msg[idx+1:],
+					colorBold,
+				)
+			}
+		}
+		return ""
+	}
+}
+
+func prettyFieldNameFormatter() zerolog.Formatter {
+	return func(i any) string {
+		return fmt.Sprintf(
+			"%s=",
+			colorize(fmt.Sprintf("%s", i), colorDarkGray),
+		)
+	}
+}
+
+func prettyFieldValueFormatter() zerolog.Formatter {
+	return func(i any) string {
+		return colorize(fmt.Sprintf("%s", i), colorYellow)
+	}
+}
+
+func prettyErrFieldNameFormatter() zerolog.Formatter {
+	return func(i any) string {
+		return fmt.Sprintf(
+			"%s=",
+			colorize(fmt.Sprintf("%s", i), colorRed),
+		)
+	}
+}
+
+func prettyErrFieldValueFormatter() zerolog.Formatter {
+	return func(i any) string {
+		return fmt.Sprintf("%s", i)
+	}
+}
+
+// Adapted from:
+// https://github.com/rs/zerolog/blob/762546b5c64e03f3d23f867213e80aa45906aaf7/console.go
+func colorize(s any, c int) string {
+	return fmt.Sprintf("\x1b[%dm%v\x1b[0m", c, s)
+}
+
+func invert(s any) string {
+	return fmt.Sprintf("\x1b[7m%s\x1b[27m", s)
+}
+
+func colorizeLevel(s any, c int) string {
+	return invert(colorize(colorize(fmt.Sprintf(" %s ", s), c), colorBold))
+}
+
+// Copied from:
+// https://github.com/rs/zerolog/blob/762546b5c64e03f3d23f867213e80aa45906aaf7/console.go
+const (
+	colorBlack = iota + 30
+	colorRed
+	colorGreen
+	colorYellow
+	colorBlue
+	colorMagenta
+	colorCyan
+	colorWhite
+
+	colorBold     = 1
+	colorDarkGray = 90
+)
 
 func inDocker() bool {
 	_, errStat := os.Stat("/.dockerenv")
