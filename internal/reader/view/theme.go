742a4f02141e2f658948004ef73919cbb41623f2
Wibowo Arindrarto <contact@arindrarto.dev>
2023-12-29 05:55:17 +0100

    refactor(reader): Set theme fields to private

--- internal/reader/view/theme.go
+++ internal/reader/view/theme.go
@@ -18,100 +18,102 @@ func LoadTheme(name string) (*Theme, error) {
 }
 
 type Theme struct {
-	Background tcell.Color
+	bg tcell.Color
 
-	BorderForeground       tcell.Color
-	BorderForegroundNormal tcell.Color
-	BorderForegroundDim    tcell.Color
+	borderFG       tcell.Color
+	borderNormalFG tcell.Color
+	borderDimFG    tcell.Color
 
-	TitleForeground       tcell.Color
-	TitleForegroundNormal tcell.Color
-	TitleForegroundDim    tcell.Color
+	titleFG       tcell.Color
+	titleNormalFG tcell.Color
+	titleDimFG    tcell.Color
 
-	FeedsGroup       tcell.Color
-	FeedsGroupNormal tcell.Color
-	FeedsGroupDim    tcell.Color
+	feedsGroup       tcell.Color
+	feedsGroupNormal tcell.Color
+	feedsGroupDim    tcell.Color
 
-	LastPullForeground       tcell.Color
-	LastPullForegroundNormal tcell.Color
-	LastPullForegroundDim    tcell.Color
+	lastPullFG       tcell.Color
+	lastPullNormalFG tcell.Color
+	lastPullDimFG    tcell.Color
 
-	EventInfoForeground       tcell.Color
-	EventInfoForegroundNormal tcell.Color
-	EventInfoForegroundDim    tcell.Color
+	eventInfoFG       tcell.Color
+	eventInfoNormalFG tcell.Color
+	eventInfoDimFG    tcell.Color
 
-	EventWarnForeground       tcell.Color
-	EventWarnForegroundNormal tcell.Color
-	EventWarnForegroundDim    tcell.Color
+	eventWarnFG       tcell.Color
+	eventWarnNormalFG tcell.Color
+	eventWarnDimFG    tcell.Color
 
-	EventErrForeground       tcell.Color
-	EventErrForegroundNormal tcell.Color
-	EventErrForegroundDim    tcell.Color
+	eventErrFG       tcell.Color
+	eventErrNormalFG tcell.Color
+	eventErrDimFG    tcell.Color
 
-	PopupTitleForeground  tcell.Color
-	PopupBorderForeground tcell.Color
+	popupTitleFG  tcell.Color
+	popupBorderFG tcell.Color
 
 	WideViewMinWidth int
 }
 
-func (theme *Theme) Dim() {
-	theme.BorderForeground = theme.BorderForegroundDim
-	theme.TitleForeground = theme.TitleForegroundDim
-	theme.LastPullForeground = theme.LastPullForegroundDim
-	theme.EventInfoForeground = theme.EventInfoForegroundDim
-	theme.FeedsGroup = theme.FeedsGroupDim
+// nolint:unused
+func (theme *Theme) dim() {
+	theme.borderFG = theme.borderDimFG
+	theme.titleFG = theme.titleDimFG
+	theme.lastPullFG = theme.lastPullDimFG
+	theme.eventInfoFG = theme.eventInfoDimFG
+	theme.feedsGroup = theme.feedsGroupDim
 }
 
-func (theme *Theme) Normalize() {
-	theme.BorderForeground = theme.BorderForegroundNormal
-	theme.TitleForeground = theme.TitleForegroundNormal
-	theme.LastPullForeground = theme.LastPullForegroundNormal
-	theme.EventInfoForeground = theme.EventInfoForegroundNormal
-	theme.FeedsGroup = theme.FeedsGroupNormal
+// nolint:unused
+func (theme *Theme) normalize() {
+	theme.borderFG = theme.borderNormalFG
+	theme.titleFG = theme.titleNormalFG
+	theme.lastPullFG = theme.lastPullNormalFG
+	theme.eventInfoFG = theme.eventInfoNormalFG
+	theme.feedsGroup = theme.feedsGroupNormal
 }
 
 //nolint:unused
 func (theme *Theme) lineStyle() tcell.Style {
 	return tcell.StyleDefault.
-		Background(theme.Background).
-		Foreground(theme.BorderForeground)
+		Background(theme.bg).
+		Foreground(theme.borderFG)
 }
 
 const darkForegroundDim = tcell.ColorDimGray
 
 var DarkTheme = &Theme{
-	Background: tcell.ColorBlack,
+	bg: tcell.ColorBlack,
 
-	BorderForeground:       tcell.ColorWhite,
-	BorderForegroundNormal: tcell.ColorWhite,
-	BorderForegroundDim:    darkForegroundDim,
+	borderFG:       tcell.ColorWhite,
+	borderNormalFG: tcell.ColorWhite,
+	borderDimFG:    darkForegroundDim,
 
-	TitleForeground:       tcell.ColorYellow,
-	TitleForegroundNormal: tcell.ColorYellow,
-	TitleForegroundDim:    darkForegroundDim,
+	titleFG:       tcell.ColorYellow,
+	titleNormalFG: tcell.ColorYellow,
+	titleDimFG:    darkForegroundDim,
 
-	FeedsGroup:       tcell.ColorGrey,
-	FeedsGroupNormal: tcell.ColorGrey,
-	FeedsGroupDim:    darkForegroundDim,
+	feedsGroup:       tcell.ColorGrey,
+	feedsGroupNormal: tcell.ColorGrey,
+	feedsGroupDim:    darkForegroundDim,
 
-	LastPullForeground:       tcell.ColorGray,
-	LastPullForegroundNormal: tcell.ColorGray,
-	LastPullForegroundDim:    darkForegroundDim,
+	lastPullFG:       tcell.ColorGray,
+	lastPullNormalFG: tcell.ColorGray,
+	lastPullDimFG:    darkForegroundDim,
 
-	EventInfoForeground:       tcell.ColorOliveDrab,
-	EventInfoForegroundNormal: tcell.ColorOliveDrab,
-	EventInfoForegroundDim:    darkForegroundDim,
+	eventInfoFG:       tcell.ColorOliveDrab,
+	eventInfoNormalFG: tcell.ColorOliveDrab,
+	eventInfoDimFG:    darkForegroundDim,
 
-	EventWarnForeground:       tcell.ColorDarkGoldenrod,
-	EventWarnForegroundNormal: tcell.ColorDarkGoldenrod,
-	EventWarnForegroundDim:    darkForegroundDim,
+	eventWarnFG:       tcell.ColorDarkGoldenrod,
+	eventWarnNormalFG: tcell.ColorDarkGoldenrod,
+	eventWarnDimFG:    darkForegroundDim,
 
-	EventErrForeground:       tcell.ColorTomato,
-	EventErrForegroundNormal: tcell.ColorTomato,
-	EventErrForegroundDim:    darkForegroundDim,
+	eventErrFG:       tcell.ColorTomato,
+	eventErrNormalFG: tcell.ColorTomato,
+	eventErrDimFG:    darkForegroundDim,
 
-	PopupBorderForeground: tcell.ColorGray,
-	PopupTitleForeground:  tcell.ColorAqua,
+	popupBorderFG: tcell.ColorGray,
+	popupTitleFG:  tcell.ColorAqua,
 
 	WideViewMinWidth: 150,
 }
