9eea4f0d3eaf77da95e49e31303a91a40367d7a6
Wibowo Arindrarto <contact@arindrarto.dev>
2024-01-13 07:21:44 +0100

    chore(reader): Reorder test for readability

--- internal/reader/ui/display_operator_test.go
+++ internal/reader/ui/display_operator_test.go
@@ -38,52 +38,6 @@ func TestClearStatusBar(t *testing.T) {
 	r.Empty(w.GetText(true))
 }
 
-func TestToggleAboutPopup(t *testing.T) {
-	a := assert.New(t)
-	r := require.New(t)
-	draw, opr, dsp := setupDisplayOperatorTest(t)
-	be := NewMockBackend(gomock.NewController(t))
-
-	draw()
-
-	name, item := dsp.root.GetFrontPage()
-	a.Equal(mainPageName, name)
-	r.Equal(dsp.mainPage, item)
-	a.Nil(dsp.aboutPopup.content)
-
-	bn1 := uuid.NewString()
-	be.EXPECT().String().Return(bn1)
-
-	opr.ToggleAboutPopup(dsp, be)
-	name, item = dsp.root.GetFrontPage()
-	a.Equal(aboutPageName, name)
-	r.Equal(dsp.aboutPopup, item)
-	r.NotNil(dsp.aboutPopup.content)
-	c1, typeok1 := dsp.aboutPopup.content.(*tview.TextView)
-	r.True(typeok1)
-	a.Contains(c1.GetText(true), bn1)
-
-	be.EXPECT().String().Times(0)
-
-	opr.ToggleAboutPopup(dsp, be)
-	name, item = dsp.root.GetFrontPage()
-	a.Equal(mainPageName, name)
-	r.Equal(dsp.mainPage, item)
-	a.NotNil(dsp.aboutPopup.content)
-
-	bn2 := uuid.NewString()
-	be.EXPECT().String().Return(bn2)
-
-	opr.ToggleAboutPopup(dsp, be)
-	name, item = dsp.root.GetFrontPage()
-	a.Equal(aboutPageName, name)
-	r.Equal(dsp.aboutPopup, item)
-	r.NotNil(dsp.aboutPopup.content)
-	c2, typeok2 := dsp.aboutPopup.content.(*tview.TextView)
-	r.True(typeok2)
-	a.Contains(c2.GetText(true), bn2)
-}
-
 func TestFocusEntriesPane(t *testing.T) {
 	r := require.New(t)
 	draw, opr, dsp := setupDisplayOperatorTest(t)
@@ -164,6 +118,72 @@ func TestFocusFeedPane(t *testing.T) {
 	r.Equal(dsp.feedsPane, dsp.inner.GetFocus())
 }
 
+func TestShowIntroPopup(t *testing.T) {
+	a := assert.New(t)
+	r := require.New(t)
+	draw, opr, dsp := setupDisplayOperatorTest(t)
+
+	// This call before draw() is intentional and simulates expected use.
+	opr.ShowIntroPopup(dsp)
+
+	draw()
+
+	name, item := dsp.root.GetFrontPage()
+	a.Equal(introPageName, name)
+	r.Equal(dsp.introPopup, item)
+
+	opr.UnfocusFront(dsp)
+	name, item = dsp.root.GetFrontPage()
+	a.Equal(mainPageName, name)
+	r.Equal(dsp.mainPage, item)
+}
+
+func TestToggleAboutPopup(t *testing.T) {
+	a := assert.New(t)
+	r := require.New(t)
+	draw, opr, dsp := setupDisplayOperatorTest(t)
+	be := NewMockBackend(gomock.NewController(t))
+
+	draw()
+
+	name, item := dsp.root.GetFrontPage()
+	a.Equal(mainPageName, name)
+	r.Equal(dsp.mainPage, item)
+	a.Nil(dsp.aboutPopup.content)
+
+	bn1 := uuid.NewString()
+	be.EXPECT().String().Return(bn1)
+
+	opr.ToggleAboutPopup(dsp, be)
+	name, item = dsp.root.GetFrontPage()
+	a.Equal(aboutPageName, name)
+	r.Equal(dsp.aboutPopup, item)
+	r.NotNil(dsp.aboutPopup.content)
+	c1, typeok1 := dsp.aboutPopup.content.(*tview.TextView)
+	r.True(typeok1)
+	a.Contains(c1.GetText(true), bn1)
+
+	be.EXPECT().String().Times(0)
+
+	opr.ToggleAboutPopup(dsp, be)
+	name, item = dsp.root.GetFrontPage()
+	a.Equal(mainPageName, name)
+	r.Equal(dsp.mainPage, item)
+	a.NotNil(dsp.aboutPopup.content)
+
+	bn2 := uuid.NewString()
+	be.EXPECT().String().Return(bn2)
+
+	opr.ToggleAboutPopup(dsp, be)
+	name, item = dsp.root.GetFrontPage()
+	a.Equal(aboutPageName, name)
+	r.Equal(dsp.aboutPopup, item)
+	r.NotNil(dsp.aboutPopup.content)
+	c2, typeok2 := dsp.aboutPopup.content.(*tview.TextView)
+	r.True(typeok2)
+	a.Contains(c2.GetText(true), bn2)
+}
+
 func TestToggleHelpPopup(t *testing.T) {
 	a := assert.New(t)
 	r := require.New(t)
@@ -222,26 +242,6 @@ func TestToggleStatsPopup(t *testing.T) {
 	a.Contains(c.GetText(true), "Unread: 5")
 
 	opr.ToggleStatsPopup(dsp, be)
-	name, item = dsp.root.GetFrontPage()
-	a.Equal(mainPageName, name)
-	r.Equal(dsp.mainPage, item)
-}
-
-func TestShowIntroPopup(t *testing.T) {
-	a := assert.New(t)
-	r := require.New(t)
-	draw, opr, dsp := setupDisplayOperatorTest(t)
-
-	// This call before draw() is intentional and simulates expected use.
-	opr.ShowIntroPopup(dsp)
-
-	draw()
-
-	name, item := dsp.root.GetFrontPage()
-	a.Equal(introPageName, name)
-	r.Equal(dsp.introPopup, item)
-
-	opr.UnfocusFront(dsp)
 	name, item = dsp.root.GetFrontPage()
 	a.Equal(mainPageName, name)
 	r.Equal(dsp.mainPage, item)
