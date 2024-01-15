36bd67372917e4977acb439261ffdff1a4ad3ff0
Wibowo Arindrarto <contact@arindrarto.dev>
2024-01-15 06:12:21 +0100

    test(backend): Add ListFeedsF tests

--- internal/reader/backend/rpc_test.go
+++ internal/reader/backend/rpc_test.go
@@ -7,10 +7,12 @@ import (
 	"context"
 	"fmt"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"go.uber.org/mock/gomock"
+	"google.golang.org/protobuf/types/known/timestamppb"
 
 	"github.com/bow/neon/api"
 )
@@ -43,6 +45,60 @@ func TestGetStatsFErr(t *testing.T) {
 
 	stats, err := rpc.GetStatsF(context.Background())()
 	r.Nil(stats)
+	a.EqualError(err, "nope")
+}
+
+func TestListFeedsFOk(t *testing.T) {
+	r := require.New(t)
+	a := assert.New(t)
+	rpc, client := newBackendRPCTest(t)
+
+	client.EXPECT().
+		ListFeeds(gomock.Any(), gomock.Any()).
+		Return(
+			&api.ListFeedsResponse{
+				Feeds: []*api.Feed{
+					{
+						Id: uint32(5),
+						Title: "F1",
+						FeedUrl: "https://f1.com/feed.xml",
+						SubTime: timestamppb.New(time.Now()),
+						LastPullTime: timestamppb.New(time.Now()),
+					},
+					{
+						Id: uint32(8),
+						Title: "F3",
+						FeedUrl: "https://f3.com/feed.xml",
+						SubTime: timestamppb.New(time.Now()),
+						LastPullTime: timestamppb.New(time.Now()),
+					},
+				},
+			},
+			nil,
+		)
+
+	feeds, err := rpc.ListFeedsF(context.Background())()
+	r.NoError(err)
+	a.Len(feeds, 2)
+	a.Equal(uint32(5), feeds[0].ID)
+	a.Equal("F1", feeds[0].Title)
+	a.Equal("https://f1.com/feed.xml", feeds[0].FeedURL)
+	a.Equal(uint32(8), feeds[1].ID)
+	a.Equal("F3", feeds[1].Title)
+	a.Equal("https://f3.com/feed.xml", feeds[1].FeedURL)
+}
+
+func TestListFeedsFErr(t *testing.T) {
+	r := require.New(t)
+	a := assert.New(t)
+	rpc, client := newBackendRPCTest(t)
+
+	client.EXPECT().
+		ListFeeds(gomock.Any(), gomock.Any()).
+		Return(nil, fmt.Errorf("nope"))
+
+	feeds, err := rpc.ListFeedsF(context.Background())()
+	r.Nil(feeds)
 	a.EqualError(err, "nope")
 }
 
